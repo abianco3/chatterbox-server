6898cee4285810e080235c049b92b1afd171abbd
abianco3 <anthony.bianco3@gmail.com>
2017-02-27 16:38:48 -0800

    Server sends back an object on request

---
+++ client/client/scripts/app.js
@@ -0,0 +1,131 @@
+// YOUR CODE HERE:
+//http://parse.sfm8.hackreactor.com/chatterbox/classes/messages
+
+const app = {
+  server: 'http://127.0.0.1:3000/',
+  foundRooms: {},
+  params: {order: '-createdAt'}
+};
+
+app.init = function() {
+  this.fetch(this.params);
+  var urlParams = window.location.search.split('=');
+  window.username = urlParams[urlParams.length - 1];
+  setInterval(() => { this.fetch(this.params); }, 20000);
+  console.log(username);
+};
+
+
+app.send = function(message) {
+  $.ajax({
+    url: 'http://127.0.0.1:3000/',
+    type: 'POST',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function(data) {
+      $('#chats').prepend(app.renderMessage(message));
+      app.renderMessage(message);
+      console.log('send worked', data);
+    },
+    error: function(data) {
+      console.error('message failed?', data);
+    }
+  });
+};
+
+app.fetch = function(params) {
+  $.ajax({
+    url: 'http://127.0.0.1:3000/',
+    type: 'GET',
+    contentType: 'application/json',
+    data: params,
+    success: (data) => {
+      console.log('fetch worked', data);
+      this.clearMessages();
+      data.results.forEach((message, index) => {
+        $('#chats').append(this.renderMessage(message));
+      });
+      this.roomPopulate(data);
+    },
+    error: function(data) {
+      console.error('failed ', data);
+    }
+  });
+};
+
+app.clearMessages = function() {
+  $('#chats').children().remove();
+};
+
+app.renderMessage = function(message) {
+  // var newUsername = '<div class="chat username">' + message.username + '</div>';
+  // var newMessage = '<div class="chat">' + newUsername + message.text + '</div>';
+  var $tweet = $('<div class="chat"></div>');
+  var $username = $('<h4 class="username"></h4>');
+  $username.append(document.createTextNode(message.username));
+  $username.addClass(message.username);
+  var $text = $('<p></p>');
+  $text.append(document.createTextNode(message.text));
+  $tweet.append($username);
+  $tweet.append($text);
+  $('.username').on('click', app.handleUsernameClick);
+  
+  return $tweet;
+};
+
+app.renderRoom = function(roomName) {
+  var $option = $(`<option>${roomName}</option>`);
+  $option.attr('value', roomName);
+  $('#roomSelect').append($option);
+};
+
+app.handleUsernameClick = function() {
+  // var $class = $(this).attr('class');
+  // console.log($class);
+  // //$(this).addClass('friend');
+  // $('.' + $class).addClass('friend');
+
+  var grabClasses = $(this).attr('class');
+  var split = grabClasses.split(' ');
+  var name = '.' + split[1];
+  $(name).addClass('friend');
+
+};
+
+app.handleSubmit = function(event) {
+  var message = {};
+  message.username = window.username;
+  message.text = $('#message').val();
+  message.roomname = $('#roomSelect').val();
+  this.send(message);
+  return false;
+};
+
+app.roomPopulate = function(data) {
+  var messages = data.results;
+  messages.forEach((obj, i, collection) => {
+    if (this.foundRooms[obj.roomname] === undefined) {
+      this.foundRooms[obj.roomname] = obj.roomname;
+      this.renderRoom(obj.roomname);
+    }
+  });
+};
+
+app.selectRoom = function(roomname) {
+  this.params.where = '{"roomname": "' + roomname + '"}';
+  this.fetch(this.params);
+};
+
+document.addEventListener('DOMContentLoaded', function() {
+  $('.submit').on('click', this.handleSubmit.bind(this));
+  this.init();
+  $('.roomAdd').on('click', () => { 
+    var roomName = $('#room').val();
+    this.renderRoom(roomName);
+  });
+  $('#roomSelect').on('change', () => {
+    var roomName = $('#roomSelect').val();
+    this.selectRoom(roomName);
+  });
+}.bind(app));
+
--- server/request-handler.js
+++ server/request-handler.js
@@ -12,7 +12,7 @@ this file and include it in basic-server.js so that it actually works.
 
 **************************************************************/
 
-var requestHandler = function(request, response) {
+module.exports = function(request, response) {
   // Request and Response come from node's http module.
   //
   // They include information about both the incoming request, such as
@@ -28,23 +28,54 @@ var requestHandler = function(request, response) {
   // debugging help, but you should always be careful about leaving stray
   // console.logs in your code.
   console.log('Serving request type ' + request.method + ' for url ' + request.url);
-
+  
+  var headers = request.headers;
+  var method = request.method;
+  var url = request.url;
+  var body = [];
   // The outgoing status.
-  var statusCode = 200;
 
   // See the note below about CORS headers.
-  var headers = defaultCorsHeaders;
 
   // Tell the client we are sending them plain text.
   //
   // You will need to change this if you are sending something
   // other than plain text, like JSON or HTML.
-  headers['Content-Type'] = 'text/plain';
 
   // .writeHead() writes to the request line and headers of the response,
   // which includes the status and all headers.
-  response.writeHead(statusCode, headers);
+  
+  request.on('error', function(err) {
+    console.log(err);
+  }).on('data', function(chunk) {
+    body.push(chunk);
+  }).on('end', function() {
 
+    response.on('error', function(err) {
+      console.error(err);
+    });
+    
+    var defaultCorsHeaders = {
+      'access-control-allow-origin': '*',
+      'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
+      'access-control-allow-headers': 'content-type, accept',
+      'access-control-max-age': 10 // Seconds.
+    };
+    
+    var statusCode = 200;
+    var responseHeaders = defaultCorsHeaders;
+    responseHeaders['Content-Type'] = 'application/json';
+    response.writeHead(statusCode, responseHeaders);
+    
+    var responseBody = {
+      headers: headers,
+      method: method,
+      url: url,
+      results: body
+    };
+
+    response.end(JSON.stringify(responseBody));
+  });
   // Make sure to always call response.end() - Node may not send
   // anything back to the client until you do. The string you pass to
   // response.end() will be the body of the response - i.e. what shows
@@ -52,7 +83,6 @@ var requestHandler = function(request, response) {
   //
   // Calling .end "flushes" the response's internal buffer, forcing
   // node to actually send all the data over to the client.
-  response.end('Hello, World!');
 };
 
 // These headers will allow Cross-Origin Resource Sharing (CORS).
@@ -64,10 +94,4 @@ var requestHandler = function(request, response) {
 //
 // Another way to get around this restriction is to serve you chat
 // client from this domain by setting up static file serving.
-var defaultCorsHeaders = {
-  'access-control-allow-origin': '*',
-  'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
-  'access-control-allow-headers': 'content-type, accept',
-  'access-control-max-age': 10 // Seconds.
-};
 
